2d58f48d4c6fe5b9641e625f597763c12eaa03bc
andres manrique <manriqueandres1104@gmail.com>
2024-06-13 02:03:29 -0500

    nuevos avances

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { NavLink } from 'react-router-dom';
+import Banner from './Banner';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <div>
+      <Banner/>
+      <Navbar bg="primary" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Arte Libre</Navbar.Brand>
+      <Navbar.Brand href="/">
+          <img
+            src={`${process.env.PUBLIC_URL}/logoArtNarinho.png`}
+            width="80"
+            height="80"
+            className="d-inline-block align-top"
+            alt="Logo de tu sitio"
+          />
+         {/*  ArtNariño */}
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <LinkContainer to="/">
-              <Nav.Link>Inicio</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Nav.Link>Acerca de</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/products">
-              <Nav.Link>Productos</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/contact">
-              <Nav.Link>Contacto</Nav.Link>
-            </LinkContainer>
+            <Nav.Link as={NavLink} to="/" exact='true'>Inicio</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">Acerca de</Nav.Link>
+            <Nav.Link as={NavLink} to="/products">Productos</Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    
+    </div>
+    
+
   );
 };
 
 export default NavigationBar;
+
