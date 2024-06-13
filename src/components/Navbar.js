a72b9e97effc5a7b613a0abd6fdee93b1b40d256
andres manrique <manriqueandres1104@gmail.com>
2024-06-13 15:20:48 -0500

    cambios navbar and mockdata

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container,FormControl, Button } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import Banner from './Banner';
 
@@ -7,7 +7,7 @@ const NavigationBar = () => {
   return (
     <div>
       <Banner/>
-      <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar bg="primary" variant="dark" expand="lg" style={{background: "#e3f2fd"}}>
       <Container>
       <Navbar.Brand href="/">
           <img
@@ -22,10 +22,16 @@ const NavigationBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
+            {/* Barra de búsqueda */}
+            <FormControl type="text" placeholder="Buscar" className="me-2" />
+           
+            {/* Otros elementos del menú de navegación */}
             <Nav.Link as={NavLink} to="/" exact='true'>Inicio</Nav.Link>
             <Nav.Link as={NavLink} to="/about">Acerca de</Nav.Link>
             <Nav.Link as={NavLink} to="/products">Productos</Nav.Link>
             <Nav.Link as={NavLink} to="/contact">Contacto</Nav.Link>
+             {/* Botón de inicio de sesión */}
+             <Button variant="outline-light" className="ms-2"  style={{ marginLeft: 'auto' }}>Iniciar sesión</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
